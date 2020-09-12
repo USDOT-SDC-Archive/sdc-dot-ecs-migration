3828ca90bc9fca8ad2bf98a437088b9e321427dd
vmayorskiy-ac <vmayorskiy@acrotron.com>
2020-09-12 10:40:15 -0400

    Updating ami sharing to include sharing of underlying snapshots

--- src/share_images.py
+++ src/share_images.py
@@ -26,6 +26,35 @@ def share_amis(client, lst, TargetAccount, DryRun=True):
     return res_lst
 
 
+def get_snapshots(client, lst, DryRun=True):
+    snapshots = []
+
+    for elt in lst:
+        image_id = elt[3]
+        images = client.describe_images(ImageIds = [image_id],
+                                     DryRun = DryRun)
+        for ami in images["Images"]:
+            for ebs in ami["BlockDeviceMappings"]:
+                snapshots.append(ebs["Ebs"]["SnapshotId"])
+
+    return snapshots
+
+
+def share_snapshots(client, snapshots, TargetAccount, DryRun=True):
+    res_lst = []
+
+    for snapshot_id in snapshots:
+        res = client.modify_snapshot_attribute(Attribute = 'createVolumePermission',
+                                               SnapshotId = snapshot_id,
+                                               OperationType = 'add',
+                                               UserIds = [TargetAccount],
+                                               DryRun = DryRun)
+        res_lst.append(res)
+
+
+    return res_lst
+
+
 # main
 def main():
     vars = awsutils.read_vars()
@@ -34,11 +63,14 @@ def main():
     with open('input/copied_amis.txt') as infile:
         lst = json.load(infile)
 
-    #pprint.pprint(vars['KmsKeyId'])
-
     res = share_amis(client, lst, DryRun=False,
                             TargetAccount=vars['TargetAccount'])
+    pprint.pprint(res)
 
+    snapshots = get_snapshots(client, lst, DryRun=False)
+    pprint.pprint(snapshots)
+
+    res = share_snapshots(client, snapshots, vars['TargetAccount'], DryRun=False)
     pprint.pprint(res)
 
 
