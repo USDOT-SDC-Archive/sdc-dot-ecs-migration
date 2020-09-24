f0749cdadda9256ddbeb7de67a3e263e5859916c
Vyacheslav Mayorskiy <vmayorskiy@prod.sdc.dot.gov>
2020-09-24 00:52:43 +0000

    Create policy implementation

---
+++ src/iam/create_roles.py
@@ -0,0 +1,61 @@
+import pprint
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+from src.utils import utils
+from src.utils import awsutils
+from src.ec2 import describe_instances
+
+
+def create_policies(client, vars):
+    with open('input/dest-policies.txt') as infile:
+        dct = json.load(infile)
+
+    res_policies = {}
+    for k, v in dct.items():
+        # TODO: move this replacement into prep_roles_policies.py
+        name = k.replace(vars['SourceRolePolicyPrefix'],
+                         vars['TargetRolePolicyPrefix'])
+
+        try:
+            policyDocument = json.dumps(v['Document'])
+            description = ''
+            if 'Description' in v.keys():
+                description = v['Description']
+
+            #pprint.pprint(name + ' --> ' + description)
+            res = client.create_policy(PolicyName=name,
+                                       PolicyDocument=policyDocument,
+                                       Description=description)
+
+            res_policies[name] = res['Policy']
+            pprint.pprint(res['Policy'])
+
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'EntityAlreadyExists':
+                pprint.pprint('WARNING: policy already exists: ' + name)
+            else:
+                raise e
+
+    return res_policies
+
+
+def create_roles(client, vars):
+    return None
+
+def main():
+    vars = awsutils.read_vars()
+    iam = boto3.client('iam')
+    res = create_roles(iam, vars)
+
+    #pprint.pprint(json.dumps(res), indent=4)
+    
+    return res
+
+
+
+if __name__ == '__main__':
+    main()
+
+
--- src/iam/prep_roles_policies.py
+++ src/iam/prep_roles_policies.py
@@ -15,6 +15,39 @@ def prep_roles(roles):
     return res
 
 
+def prep_policies_NOT_FINISHED(policies):
+    vars = awsutils.read_vars()
+    srcPrefix = vars['SourceAccount']
+    destPrefix = 'DELETE_ME'  # vars['TargetAccount']
+
+    s3map = utils.map_s3_to_ecs_prod()
+
+    dct = json.loads(policies)
+    for policyName, policyElement in dct.items():
+        destPolicyElement = policyElement
+        destStatement = []
+        for elt in policyElement['Document']['Statement']:
+            resource = elt['Resource']
+
+            if isinstance(resource, list):
+                destResource = []
+                for r in resource:
+                    found = False
+                    for srcS3, destS3 in s3map.items():
+                        if srcS3 in r:
+                            found = True
+                            destResource.append(r.replace(srcS3, destS3))
+                        #res = res.replace(k, v)
+                        # TODO: cleanup non-existing resources
+                        # will have to work with the actual structure
+
+                elt = destResource
+
+
+
+    return json.dumps(dct, indent=4)
+
+
 def prep_policies(policies):
     s3map = utils.map_s3_to_ecs_prod()
     res = policies
@@ -23,6 +56,10 @@ def prep_policies(policies):
         # TODO: cleanup non-existing resources
         # will have to work with the actual structure
 
+    vars = awsutils.read_vars()
+    src = vars['SourceAccount']
+    dest = 'DELETE_ME'  # vars['TargetAccount']
+    res = res.replace(src, dest)
     return res
 
 
